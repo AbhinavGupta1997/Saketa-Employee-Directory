97e01ccdeb0f6777d12957b4bd0bd87863ebf78b
Abhinav Gupta <abhinavsarkar1997@gmail.com>
2021-06-25 19:33:21 +0530

    changed API names

--- src/app/Configs/auth.config.ts
+++ src/app/Configs/auth.config.ts
@@ -15,7 +15,7 @@ export const authConfig: AuthConfig = {
 
   // set the scope for the permissions the client should request
   // The first three are defined by OIDC. The 4th is a usecase-specific one
-  scope: 'openid profile offline_access api1', 
+  scope: 'openid profile offline_access AddressBookAPI', 
 
   requestAccessToken: true,
 
